bd61d3a7508ba105771f89a97e5cba96b85ad54d
Dyeed <gggssn@qq.com>
2023-08-09 10:54:23 +0800

    初始化版本，可以本地静态预览

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,28 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const fs = require("fs");
+const path = require("path");
+const homedir = require('os').homedir()
+
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  devServer: {
+    port: 3000
+    ,
+    https: {
+      key: fs.readFileSync(path.resolve(`${homedir}/.office-addin-dev-certs/localhost.key`)),
+      cert: fs.readFileSync(path.resolve(`${homedir}/.office-addin-dev-certs/localhost.crt`)),
+      ca: fs.readFileSync(path.resolve(`${homedir}/.office-addin-dev-certs/ca.crt`)),
+    }
+    // ,
+    // plugins:{
+    //     new CopyWebpackPlugin([{
+    //       from:"./public",
+    //       to:"public"
+    //     }])
+    // }
+  }
+}
